52e29018d91a941f86440113aefc4d111bdc5982
pierre4cf <155096082+pierre4cf@users.noreply.github.com>
2025-03-22 15:37:08 +0100

    adding a Definition of Comment tables

--- src/content/config.ts
+++ src/content/config.ts
@@ -63,6 +63,19 @@ const postCollection = defineCollection({
   }),
 });
 
+<!-- Defining Comment tables-->
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Comment = defineTable({
+  columns: {
+    author: column.text(),
+    body: column.text(),
+  }
+})
+
+export default defineDb({
+  tables: { Comment },
+})
 export const collections = {
   post: postCollection,
 };
